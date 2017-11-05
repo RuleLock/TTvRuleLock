242f6236895b51e60c49853cc02b87dfd0fba23c
fxc0719 <1@fxcdev.com>
2017-11-06 00:04:43 +0800

    add utils

---
+++ RuleLock_Android_Phone/app/src/main/java/com/locker/rulelock/GlobalConst.kt
@@ -0,0 +1,7 @@
+package com.locker.rulelock
+
+/**
+ * @description
+ * @author fxc
+ * @date 17-11-6
+ */
---
+++ RuleLock_Android_Phone/app/src/main/java/com/locker/rulelock/GlobalUtils.kt
@@ -0,0 +1,8 @@
+package com.locker.rulelock
+
+/**
+ * @description
+ * @author fxc
+ * @date 17-11-6
+ */
+
